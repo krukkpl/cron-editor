2a10c73f9e816b5344b52bddc1195f1fcab9fc05
Krzysztof Kruk <krzysztof.kruk@makit.pl>
2019-09-24 10:28:43 +0200

    TranslateModule

--- projects/cron-editor/src/lib/cron-editor.module.ts
+++ projects/cron-editor/src/lib/cron-editor.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { CronEditorComponent } from './cron-editor.component';
 import { TimePickerComponent } from './time-picker/time-picker.component';
 
 @NgModule({
   declarations: [CronEditorComponent, TimePickerComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, TranslateModule.forRoot()],
   exports: [CronEditorComponent]
 })
 export class CronEditorModule { }
